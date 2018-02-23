b58cea806bf90629953aab37f180c1d60a031bfe
Alexey Savchuk <sabo@devpreview.ru>
2018-02-23 19:19:31 +0300

    serializeObject / deserializeObject methods

--- src/main/object-mapper.ts
+++ src/main/object-mapper.ts
@@ -12,14 +12,14 @@ export class ObjectMapper {
     }
 
     public static serialize<T>(obj: T | T[]): string;
-    public static serialize<T, J>(obj: T, type: Type<J>): J;
-    public static serialize<T, J>(obj: T[], type: Type<J>): J[];
-    public static serialize<T, J>(obj: T | T[], type?: Type<J>): string | J | J[] {
-        if (type !== undefined) {
+    public static serialize<T, J>(obj: T, jtype: Type<J>): J;
+    public static serialize<T, J>(obj: T[], jtype: Type<J>): J[];
+    public static serialize<T, J>(obj: T | T[], jtype?: Type<J>): string | J | J[] {
+        if (jtype !== undefined) {
             if (obj instanceof Array) {
-                return this.getInstance().serialize<T, J>(obj, type);
+                return this.getInstance().serialize<T, J>(obj, jtype);
             } else {
-                return this.getInstance().serialize<T, J>(obj, type);
+                return this.getInstance().serialize<T, J>(obj, jtype);
             }
         } else {
             return this.getInstance().serialize<T>(obj);
@@ -28,48 +28,72 @@ export class ObjectMapper {
 
     public static deserialize<T>(type: Type<T>, json: string): T;
     public static deserialize<T, J>(type: Type<T>, json: J): T;
-    public static deserialize<T>(type: Type<T>, json: any): T {
-        return this.getInstance().deserialize<T>(type, json);
+    public static deserialize<T, J>(type: Type<T>, json: string | J): T {
+        if (typeof json == 'string') {
+            return this.getInstance().deserialize<T>(type, json);
+        } else {
+            return this.getInstance().deserialize<T, J>(type, json);
+        }
     }
 
     public static deserializeArray<T>(type: Type<T>, json: string): T[];
     public static deserializeArray<T, J>(type: Type<T>, json: J[]): T[];
-    public static deserializeArray<T>(type: Type<T>, json: any): T[] {
-        return this.getInstance().deserializeArray<T>(type, json);
+    public static deserializeArray<T, J>(type: Type<T>, json: string | J[]): T[] {
+        if (typeof json == 'string') {
+            return this.getInstance().deserializeArray<T>(type, json);
+        } else {
+            return this.getInstance().deserializeArray<T, J>(type, json);
+        }
     }
 
     public constructor() {
     }
 
     public serialize<T>(obj: T | T[]): string;
-    public serialize<T, J>(obj: T, type: Type<J>): J;
-    public serialize<T, J>(obj: T[], type: Type<J>): J[];
-    public serialize<T, J>(obj: T | T[], type?: Type<J>): string | J | J[] {
+    public serialize<T, J>(obj: T, jtype: Type<J>): J;
+    public serialize<T, J>(obj: T[], jtype: Type<J>): J[];
+    public serialize<T, J>(obj: T | T[], jtype?: Type<J>): string | J | J[] {
         if (obj instanceof Array) {
-            if (type !== undefined) {
-                return [new type()];
+            if (jtype !== undefined) {
+                return obj.map(obj => this.serializeObject<T, J>(obj, jtype));
             } else {
-                return JSON.stringify([{test: 'ok'}]);
+                return JSON.stringify(obj.map(obj => this.serializeObject<T, Object>(obj, Object)));
             }
         } else {
-            if (type !== undefined) {
-                return new type();
+            if (jtype !== undefined) {
+                return this.serializeObject<T, J>(obj, jtype);
             } else {
-                return JSON.stringify({test: 'ok'});
+                return JSON.stringify(this.serializeObject<T, Object>(obj, Object));
             }
         }
     }
 
     public deserialize<T>(type: Type<T>, json: string): T;
     public deserialize<T, J>(type: Type<T>, json: J): T;
-    public deserialize<T>(type: Type<T>, json: any): T {
-        return new type();
+    public deserialize<T, J>(type: Type<T>, json: string | J): T {
+        if (typeof json == 'string') {
+            return this.deserializeObject<T, Object>(type, JSON.parse(json));
+        } else {
+            return this.deserializeObject<T, J>(type, json);
+        }
     }
 
     public deserializeArray<T>(type: Type<T>, json: string): T[];
     public deserializeArray<T, J>(type: Type<T>, json: J[]): T[];
-    public deserializeArray<T>(type: Type<T>, json: any): T[] {
-        return [new type(), new type()];
+    public deserializeArray<T, J>(type: Type<T>, json: string | J[]): T[] {
+        if (typeof json == 'string') {
+            return JSON.parse(json).map((json: Object) => this.deserializeObject<T, Object>(type, json));
+        } else {
+            return json.map((json) => this.deserializeObject<T, J>(type, json));
+        }
+    }
+
+    protected serializeObject<T, J>(obj: T, jtype: Type<J>): J {
+        return new jtype();
+    }
+
+    protected deserializeObject<T, J>(type: Type<T>, json: J): T {
+        return new type();
     }
 
 }
