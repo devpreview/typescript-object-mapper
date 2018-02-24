f3d1c32124ec79487be339c06c76035f312e6bd3
Alexey Savchuk <sabo@devpreview.ru>
2018-02-25 01:44:47 +0300

    Coverage

--- src/main/public_api.ts
+++ src/main/public_api.ts
@@ -1,3 +1,9 @@
 export { Type } from './type';
 export { ObjectMapper } from './object-mapper';
 export { ObjectMapperError } from './object-mapper-error';
+
+export {} from './decorators/json-class';
+export {} from './decorators/json-creator';
+export {} from './decorators/json-ignore';
+export {} from './decorators/json-include';
+export { JsonProperty, JsonPropertyMetadata } from './decorators/json-property';
